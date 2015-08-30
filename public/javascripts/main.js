084d7752f21441362923e94e684f6ba090b40ca7
Nicolas Joseph <nicolas@nicolasjoseph.com>
2015-08-30 19:45:43 -0400

    #28 - Enable navigation through the input in the nabber

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,5 +8,10 @@ $(document)
     $('.ui.rating')
         .rating();
 
+    $('#goToRepoButton')
+        .click(function(){
+          window.location.href = window.location.protocol + '/github/'+ $("#goToRepoInput")[0].value
+        })
+
   });
 
