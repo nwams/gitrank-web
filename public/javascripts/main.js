0d1234ed5da2cbcd230288357a5058507e9c2df6
brunnoattorre <brattorre@gmail.com>
2015-09-09 12:00:25 -0400

    #53 Finishing the linking between feedback page and backend server

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,6 +19,18 @@ $(document)
             .rating('setting', 'onRate', function (value) {
                 $('#scoreDocumentation').val(value)
             });
+        $('#matScore')
+            .rating('setting', 'onRate', function (value) {
+                $('#scoreMaturity').val(value)
+            });
+        $('#desScore')
+            .rating('setting', 'onRate', function (value) {
+                $('#scoreDesign').val(value)
+            });
+        $('#supScore')
+            .rating('setting', 'onRate', function (value) {
+                $('#scoreSupport').val(value)
+            });
 
         function goToRepoInputUrl() {
             window.location.href = window.location.protocol + '/github/' + $("#goToRepoInput")[0].value
