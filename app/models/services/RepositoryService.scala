5b72398efac5959ffc5cb8284a691f52849dd8c7
Nicolas Joseph <nicolas@nicolasjoseph.com>
2015-09-20 00:56:10 -0400

    Synchronize the contribution

--- app/models/services/RepositoryService.scala
+++ app/models/services/RepositoryService.scala
@@ -25,10 +25,17 @@ class RepositoryService @Inject()(
    * @param name name of the repo to save or update
    * @param addedLines number of added lines to the repo
    * @param removedLines number of deleted lines from the repo
+   * @param karmaWeight sum of all the karma weight
    * @param score score of the repo
    * @return the saved Repository
    */
-  def save(id: Int, name: String, addedLines: Option[Int], removedLines: Option[Int], karmaWeight: Option[Int], score: Option[Int]): Future[Repository] = {
+  def save(id: Int,
+           name: String,
+           addedLines: Option[Int],
+           removedLines: Option[Int],
+           karmaWeight: Option[Int] = None,
+           score: Option[Int] = None): Future[Repository] = {
+
     repoDAO.find(id).flatMap({
       case Some(existingRepo) =>
         repoDAO.update(existingRepo.copy(
@@ -63,8 +70,8 @@ class RepositoryService @Inject()(
       case Some(existingContribution) => {
         contributionDAO.update(userName, repoName, existingContribution.copy(
           timestamp = contribution.timestamp,
-          addedLines = existingContribution.addedLines + contribution.addedLines - parseWeekAddedLines(existingContribution.currentWeekBuffer),
-          removedLines = existingContribution.removedLines + contribution.removedLines - parseWeekDeletedLines(existingContribution.currentWeekBuffer),
+          addedLines = existingContribution.addedLines + contribution.addedLines - contributionDAO.parseWeekAddedLines(existingContribution.currentWeekBuffer),
+          removedLines = existingContribution.removedLines + contribution.removedLines - contributionDAO.parseWeekDeletedLines(existingContribution.currentWeekBuffer),
           currentWeekBuffer = contribution.currentWeekBuffer
         ))
       }
@@ -87,28 +94,6 @@ class RepositoryService @Inject()(
    * @return
    */
   def retrieve(repoId: Int): Future[Option[Repository]] = repoDAO.find(repoId)
-
-  /**
-   * Parses a string representing the buffer of the current week contribution getting the added lines
-   *
-   * @param currentWeekBuffer String containing the added lines this week already counted for needed to be extracted.
-   * @return count of the added lines already accounted for extracted as an Int
-   */
-  private def parseWeekAddedLines(currentWeekBuffer: Option[String]): Int = {
-    val str = currentWeekBuffer.getOrElse("a0d0")
-    str.substring(0, str.indexOf("d")).toInt
-  }
-
-  /**
-   * Parses a string representing the buffer of the current week contribution getting the deleted lines
-   *
-   * @param currentWeekBuffer String containing the deleted lines this week already counted for needed to be extracted.
-   * @return count of the deleted lines already accounted for extracted as an Int
-   */
-  private def parseWeekDeletedLines(currentWeekBuffer: Option[String]): Int = {
-    val str = currentWeekBuffer.getOrElse("a0d0")
-    str.substring(str.indexOf("d"), str.length).toInt
-  }
 
   /**
    * Gets all the contributors for a given repository with all their contributions
