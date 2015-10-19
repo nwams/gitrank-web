7704f02be52ee5974f3543745ccad97c462599a7
brunnoattorre <brattorre@gmail.com>
2015-10-19 10:10:34 -0400

    Removing debug log

--- app/models/daos/UserDAO.scala
+++ app/models/daos/UserDAO.scala
@@ -92,10 +92,7 @@ class UserDAO @Inject() (neo: Neo4j) {
 
     neo.cypher("CREATE (n:User {props}) RETURN n", Json.obj(
       "props" -> jsonToSend
-    )).map(response =>{
-      print(response)
-      user
-    } )
+    )).map(response =>user)
   }
 
   /**
